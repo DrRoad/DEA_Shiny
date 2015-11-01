6022a452cb00c25db4aea9076ce1368a657743ab
Kevin van Blommestein <kvanblommestein@gmail.com>
2015-11-01 12:51:48 +1100

    Adding demo Upload button, added introduction

--- server.R
+++ server.R
@@ -8,6 +8,52 @@ server <- function(session, input, output) {
   reactiveData <- reactiveValues(df = data.frame(), 
                                  res = data.frame())
   
+  observe ({
+    if (input$demo.button == 0)
+      return(NULL)
+    
+    reactiveData$df <<- read.csv(paste0(getwd(), "/data/Martino_data.csv"))
+    
+    # If there is data in the file, change setup tab to Model Setup
+    if (nrow(reactiveData$df) != 0) {
+      # Find columns with only numeric values
+      numeric.cols <- colnames(reactiveData$df)[sapply(reactiveData$df, is.numeric)]
+      # Populate select inputs for creating model
+      updateSelectInput(session, "model.dmus", "DMU Names", c("none", colnames(reactiveData$df)), selected = "none")
+      updateSelectInput(session, "model.inputs", "Input(s)", c("constant.one", numeric.cols), selected = "constant.one")
+      updateSelectInput(session, "model.outputs", "Output(s)", c("constant.one", numeric.cols))
+      updateSelectInput(session, "model.z", "Secondary z", c("none", numeric.cols))
+      # Change to model setup and data tables tabs
+      updateTabsetPanel(session, "tabset.setup", selected = "2.Model")
+      updateTabsetPanel(session, "tabset.result", selected = "Data Table")
+    }
+  })
+  
+  # Wait for File to be Uploaded
+  observe ({
+    in.file <- input$file.upload
+    if (is.null(in.file))
+      return(NULL)
+    
+    file.quote <- switch(input$file.quote, double = '"', single = "'", "")
+    reactiveData$df <<- read.csv(in.file$datapath, header = input$file.header, sep = input$file.sep, 
+                                 quote = file.quote)
+    
+    # If there is data in the file, change setup tab to Model Setup
+    if (nrow(reactiveData$df) != 0) {
+      # Find columns with only numeric values
+      numeric.cols <- colnames(reactiveData$df)[sapply(reactiveData$df, is.numeric)]
+      # Populate select inputs for creating model
+      updateSelectInput(session, "model.dmus", "DMU Names", c("none", colnames(reactiveData$df)), selected = "none")
+      updateSelectInput(session, "model.inputs", "Input(s)", c("constant.one", numeric.cols), selected = "constant.one")
+      updateSelectInput(session, "model.outputs", "Output(s)", c("constant.one", numeric.cols))
+      updateSelectInput(session, "model.z", "Secondary z", c("none", numeric.cols))
+      # Change to model setup and data tables tabs
+      updateTabsetPanel(session, "tabset.setup", selected = "2.Model")
+      updateTabsetPanel(session, "tabset.result", selected = "Data Table")
+    }
+  })
+  
   # Wait for Run Model button to be pressed before running DEA analysis
   observe ({
     if (input$model.button == 0)
@@ -24,12 +70,12 @@ server <- function(session, input, output) {
       y <- input$model.outputs
       z <- input$model.z
       
-      if ("constant.1" %in% x | length(x) == 0)
+      if ("constant.one" %in% x | length(x) == 0)
         x <- as.matrix(rep(1, nrow(reactiveData$df)), ncol = 1)
       else
         x <- reactiveData$df[, x]
       
-      if ("constant.1" %in% y | length(y) == 0)
+      if ("constant.one" %in% y | length(y) == 0)
         y <- as.matrix(rep(1, nrow(reactiveData$df)), ncol = 1)
       else
         y <- reactiveData$df[, y]
@@ -50,10 +96,11 @@ server <- function(session, input, output) {
       
       res.df <- data.frame(dmu = rownames(res$eff), eff = res$eff, status1 = res$status1, 
                            status2 = res$status2, lambda = res$lambda)
+      
       if (input$model.slack)
-        res.df <- cbind(res.df, res$sx, res$sy)
+        res.df <- cbind(res.df, sx = res$sx, sy = res$sy)
       if (input$model.dual)
-        res.df <- cbind(res.df, res$vx, res$uy, res$w)
+        res.df <- cbind(res.df, vx = res$vx, uy = res$uy, w = res$w)
       
       reactiveData$res <<- res.df
     })
@@ -66,25 +113,7 @@ server <- function(session, input, output) {
   
   # Display data when file uploaded
   output$table.data <- renderDataTable({
-    in.file <- input$file.upload
-    if (is.null(in.file))
-      return(NULL)
-    
-    file.quote <- switch(input$file.quote, double = '"', single = "'", "")
-    reactiveData$df <<- read.csv(in.file$datapath, header = input$file.header, sep = input$file.sep, 
-                                 quote = file.quote)
-    
-    # If there is data in the file, change setup tab to Model Setup
-    if (nrow(reactiveData$df) != 0) {
-      numeric.cols <- colnames(reactiveData$df)[sapply(reactiveData$df, is.numeric)]
-      updateTabsetPanel(session, "tabset.setup", selected = "2.Model")
-      updateSelectInput(session, "model.dmus", "DMU Names", c("none", colnames(reactiveData$df)), selected = "none")
-      updateSelectInput(session, "model.inputs", "Input(s)", c("constant.1", numeric.cols), selected = "constant.1")
-      updateSelectInput(session, "model.outputs", "Output(s)", c("constant.1", numeric.cols))
-      updateSelectInput(session, "model.z", "Secondary z", c("none", numeric.cols))
-      reactiveData$df
-    }
-    
+    reactiveData$df
   }, options = list(pageLength = 10, scrollX=TRUE))
   
   # Download file when download button is pressed
--- ui.R
+++ ui.R
@@ -11,6 +11,10 @@ dashboardPage(skin = "blue",
         id = "tabset.setup",
         title = tagList(shiny::icon("gear"), "Setup"),
         tabPanel("1.Data",
+                 box(id = "box.upload.adv", title = "Load Demo File", solidHeader = FALSE, collapsed = FALSE,
+                     collapsible = TRUE, width = NULL, background = "light-blue",
+                     actionButton('demo.button', 'Load Demo File', icon("table"))
+                 ),
                  box(id = "box.upload.adv", title = "Upload File", solidHeader = FALSE, collapsed = FALSE,
                      collapsible = TRUE, width = NULL, background = "light-blue",
                      fileInput('file.upload', 'Choose CSV File', accept=opts$file$type),
@@ -50,12 +54,44 @@ dashboardPage(skin = "blue",
         title = tagList(shiny::icon("line-chart"), "Results"), 
         width = NULL,
         id = "tabset.result",
+        tabPanel("Introduction",
+                 h3("Introduction"),
+                 p("Data Envelopment Analysis (DEA) was first formulated by Charnes et al. [1], 
+                 which built on earlier work by Farrell [2]. DEA is non parametric linear programming 
+                 approach used for evaluating the performance of a set of DMUs which convert multiple 
+                 inputs into multiple outputs. It is useful for cases where the relationships between 
+                 the inputs and outputs are complex or unknown and it does not require any prior 
+                 assumptions. As opposed to regression, which fits a line through the center of the data, 
+                 DEA creates a piecewise linear curve on top of the observations [3]."),
+                 # br(),
+                 h5("Formulation for Input-Oriented Variable Returns to Scale (VRS) DEA model, including slack variables:"),
+                 withMathJax(),
+                 p("$$\\text{minimize  }\\theta$$
+                   $$\\sum_{j=1}^{n} x_{i,j}\\lambda_j - \\theta x_{i,k} + s^x_i = 0 \\; \\forall i$$
+                   $$\\sum_{j=1}^{n} y_{r,j}\\lambda_j - s^y =  y_{r,k} \\; \\forall r$$
+                   $$\\text{subject to }\\sum_{j=1}^{n} \\lambda_j  = 1$$
+                   $$\\lambda_j , s^x_i, s^y_r \\geq 0  \\; \\forall i, \\; r, \\; j$$"),
+                 
+                 h4("References"),
+                 tags$ol(
+                   tags$li('M. J. Farrell, "The Measurement of Productive Efficiency", vol. 120, no. 3, pp. 253-290, 1957.'),
+                   tags$li('W. W. Cooper, L. M. Seiford, and J. Zhu, "Handbook on Data Envelopment Analysis", 1st ed. Kluwer Academic Publishers, 2004.'))
+        ),
         tabPanel("Data Table", 
+                 h3("Uploaded Data"),
                  dataTableOutput(outputId = "table.data")
         ),
         tabPanel("Result",
-                 downloadButton('download.result', 'Download'),
-                 dataTableOutput(outputId = "table.result")
+                 h3("Results"),
+                 p("Description of Results:",
+                 tags$ul(
+                  tags$li("In an input-oriented model, efficiency < 1 indicates inefficiency"),
+                  tags$li("In an output-oriented model, efficiency > 1 indicates inefficiency"),
+                  tags$li("status1 and status2 reflect 'error' statuses (0 = no problem)"),
+                  tags$li("lambda.xx indicates that DMU xx is used (> 0) or not used (= 0) in setting a 'target' for any of the other DMUs"))),
+                 br(),
+                 dataTableOutput(outputId = "table.result"),
+                 downloadButton('download.result', 'Download')
         )
     )
   )    
